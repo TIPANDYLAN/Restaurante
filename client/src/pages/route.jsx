23efdd9eeb47301f422692c294c4459f7f672aaf
JohanGodoy1011 <84431930+JohanGodoy1011@users.noreply.github.com>
2023-07-25 17:22:46 -0500

    fronted registrar cliente

--- client/src/pages/route.jsx
+++ client/src/pages/route.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Inicio from "./inicio";
+import Cliente from "./ciente";
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <Router>
         <Routes>
           <Route path="/" element={<Inicio/>} />
+          <Route path="/Cliente" element={<Cliente/>} />
         </Routes>
       </Router>
     </>

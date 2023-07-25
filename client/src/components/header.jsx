acacc45d2bbe5bae3a37349159f745214a5ed0ea
TIPANDYLAN <105365454+TIPANDYLAN@users.noreply.github.com>
2023-07-25 11:54:53 -0500

    Header Terminado

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 import "../styles/Header.css";
+import Lupa from "../images/search-icon.png";
+import Login from "../images/login.png"
 
-export const Header = () =>{
+export const Header = ({titulo,Buscar}) =>{
+
     return(
-        <div className="Header"/>
+        <>
+            <div className="Header"/>
+            <div className="Menu">
+                <div className="titulo">{titulo}</div>
+                <button className="login">
+                    <div className="texto">Iniciar Sesion</div>{}
+                    <img src={Login} alt="" />
+                </button>
+                <div className="Busqueda">
+                    <button><img src={Lupa} alt="" /></button>
+                    {Buscar ? <input type="text" placeholder="Buscar" className="Buscar"/> : null}
+                </div>
+            </div>
+        </>
     );
 }
--- client/src/pages/inicio.jsx
+++ client/src/pages/inicio.jsx
@@ -4,7 +4,7 @@ import {Header} from "../components/header.jsx"
 const Inicio = ()=>{
     return(
         <>
-        <Header/>
+        <Header titulo={"Menú de Platillos"} Buscar={true}/>
         <p>Hola Mundo!</p>
         </>
     );

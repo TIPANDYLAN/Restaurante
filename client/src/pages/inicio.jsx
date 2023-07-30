7198f44ec872a81105e9126c13ebe8ead303b8aa
Gwchicango <yorgech12@gmail.com>
2023-07-30 14:48:16 -0500

    Bd datos

--- client/src/pages/inicio.jsx
+++ client/src/pages/inicio.jsx
@@ -5,6 +5,10 @@ import "../styles/Inicio.css";
 
 const Inicio = () => {
   const [platos, setPlatos] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [foto, setFoto] = useState(null);
 
   useEffect(() => {
     // Hacer la solicitud a la API para obtener los platos
@@ -15,7 +19,27 @@ const Inicio = () => {
       .catch((error) => {
         console.error("Error al obtener los platos:", error);
       });
-  }, []); // El arreglo vacío [] como segundo argumento asegura que el efecto solo se ejecute una vez al montar el componente
+  }, []);
+
+  const handleAgregarPlato = () => {
+    // Crear un objeto FormData para enviar la imagen al servidor
+    const formData = new FormData();
+    formData.append("Nombre", nombre);
+    formData.append("Precio", precio);
+    formData.append("Descripcion", descripcion);
+    formData.append("Foto", foto);
+
+    // Hacer la solicitud POST a la API para crear un nuevo plato
+    axios.post("http://localhost:4000/api/platos", formData)
+      .then((response) => {
+        console.log("Plato creado exitosamente");
+        // Actualizar la lista de platos después de crear uno nuevo
+        setPlatos([...platos, response.data]);
+      })
+      .catch((error) => {
+        console.error("Error al crear el plato:", error);
+      });
+  };
 
   return (
     <>
@@ -24,12 +48,32 @@ const Inicio = () => {
         <h2>Platos disponibles:</h2>
         <ul>
           {platos.map((plato) => (
-            <li key={plato.ID}>
-              <p>Nombre: {plato.Nombre}</p>
-              <p>Precio: {plato.Precio}</p>
+            <li key={plato.ID_PL}>
+              <p>Nombre: {plato.NOMBRE_PL}</p>
+              <p>Precio: {plato.PRECIO_PL}</p>
+              <p>Descripción: {plato.DESCRIPCION_PL}</p>
+              <img src={`http://localhost:4000/images/${plato.FOTO_PL}`} alt={plato.NOMBRE_PL} />
             </li>
           ))}
         </ul>
+      </div>
+      <div>
+        <h2>Agregar nuevo plato:</h2>
+        <form>
+          <label>Nombre:</label>
+          <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+          <br />
+          <label>Precio:</label>
+          <input type="number" value={precio} onChange={(e) => setPrecio(e.target.value)} />
+          <br />
+          <label>Descripción:</label>
+          <textarea value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+          <br />
+          <label>Foto:</label>
+          <input type="file" onChange={(e) => setFoto(e.target.files[0])} />
+          <br />
+          <button type="button" onClick={handleAgregarPlato}>Agregar Plato</button>
+        </form>
       </div>
     </>
   );
--- server/App.js
+++ server/App.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
 
+// Configuración de Multer para manejar la carga de imágenes
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'kandela' // Nombre de la base de datos "kandela"
+  database: 'Restaurant' // Nombre de la base de datos "Restaurant"
 });
 
 connection.connect((error) => {
@@ -24,7 +40,7 @@ connection.connect((error) => {
 
 // Obtener todos los platos
 app.get('/api/platos', (req, res) => {
-  const query = 'SELECT * FROM kandela.plato';
+  const query = 'SELECT * FROM Restaurant.plato';
 
   connection.query(query, (error, results) => {
     if (error) {
@@ -37,17 +53,19 @@ app.get('/api/platos', (req, res) => {
 });
 
 // Crear un nuevo plato
-app.post('/api/platos', (req, res) => {
-  const { Nombre, Precio } = req.body;
-  const query = 'INSERT INTO kandela.plato (Nombre, Precio) VALUES (?, ?)';
+app.post('/api/platos', upload.single('Foto'), (req, res) => {
+  const { Nombre, Precio, Descripcion } = req.body;
+  const Foto = req.file.filename;
 
-  connection.query(query, [Nombre, Precio], (error, result) => {
+  const query = 'INSERT INTO Restaurant.plato (NOMBRE_PL, PRECIO_PL, FOTO_PL, DESCRIPCION_PL) VALUES (?, ?, ?, ?)';
+
+  connection.query(query, [Nombre, Precio, Foto, Descripcion], (error, result) => {
     if (error) {
       console.error('Error al crear el plato:', error);
       res.sendStatus(500);
     } else {
       console.log('Plato creado exitosamente');
-      res.sendStatus(200);
+      res.json({ ID_PL: result.insertId, NOMBRE_PL: Nombre, PRECIO_PL: Precio, FOTO_PL: Foto, DESCRIPCION_PL: Descripcion });
     }
   });
 });
@@ -56,7 +74,7 @@ app.post('/api/platos', (req, res) => {
 app.put('/api/platos/:id', (req, res) => {
   const id = req.params.id;
   const { Nombre, Precio } = req.body;
-  const query = 'UPDATE kandela.plato SET Nombre = ?, Precio = ? WHERE ID = ?';
+  const query = 'UPDATE Restaurant.plato SET NOMBRE_PL = ?, PRECIO_PL = ? WHERE ID_PL = ?';
 
   connection.query(query, [Nombre, Precio, id], (error, result) => {
     if (error) {
@@ -72,7 +90,7 @@ app.put('/api/platos/:id', (req, res) => {
 // Eliminar un plato por ID
 app.delete('/api/platos/:id', (req, res) => {
   const id = req.params.id;
-  const query = 'DELETE FROM kandela.plato WHERE ID = ?';
+  const query = 'DELETE FROM Restaurant.plato WHERE ID_PL = ?';
 
   connection.query(query, [id], (error, result) => {
     if (error) {
